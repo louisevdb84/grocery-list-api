b0c1ea2af5a9e0ab65d4b450708a0acb62cecf38
louisevdb84 <louisevdb84@gmail.com>
2020-12-09 14:15:10 +0100

    jk

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const expressPlayground = require("graphql-playground-middleware-express").defau
 const mongoose = require('mongoose');
 
 const mongoDbConnectionString = process.env.groceryListMongoDb;
- mongoose.connect(mongoDbConnectionString, {
+ mongoose.connect("mongodb+srv://louisevdb84:Password_123@cluster0.d6h2c.mongodb.net/shopping_list?retryWrites=true&w=majority", {
 
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -21,6 +21,7 @@ mongoose.connection.once('open', () => {
 });
 
 app.use(cors());
+mongoose.set('bufferCommands', false);
 
 
 app.use('/graphql',  graphqlHTTP({
