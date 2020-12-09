209cb2a74ed80174b62f4da9dc18d1d478360b56
louisevdb84 <louisevdb84@gmail.com>
2020-12-09 11:19:14 +0100

    heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const expressPlayground = require("graphql-playground-middleware-express").defau
 const mongoose = require('mongoose');
 
 const mongoDbConnectionString = process.env.groceryListMongoDb;
- mongoose.connect("mongodb+srv://louisevdb84:Neelix_007@cluster0.d6h2c.mongodb.net/shopping_list?retryWrites=true&w=majority", {
+ mongoose.connect(mongoDbConnectionString, {
 
     useNewUrlParser: true,
     useUnifiedTopology: true,
